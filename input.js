cd046aa76438acbc2c583b18ec2f0b53b3043233
rafimoz <87600955+rafimoz@users.noreply.github.com>
2021-11-03 23:43:56 +0600

    Update input.js
    Mobile control added

--- input.js
+++ input.js
@@ -1,3 +1,8 @@
+let upper = document.getElementById('up');
+let lefter = document.getElementById('left');
+let righter = document.getElementById('right');
+let downer = document.getElementById('down');
+
 let inputDirection = { x: 0, y: 0}
 let lastInputDirection = { x: 0, y: 0}
 window.addEventListener('keydown', e =>{
@@ -19,6 +24,22 @@ window.addEventListener('keydown', e =>{
             inputDirection = { x: 1, y: 0}
             break            
     }
+})
+upper.addEventListener("click",()=>{
+    console.log("Heloo up");
+    inputDirection = { x: 0, y: -1}
+})
+lefter.addEventListener("click",()=>{
+    console.log("Heloo left");
+    inputDirection = { x: -1, y: 0}
+})
+righter.addEventListener("click",()=>{
+    console.log("Heloo right");
+    inputDirection = { x: 1, y: 0}
+})
+downer.addEventListener("click",()=>{
+    console.log("Heloo down");
+    inputDirection = { x: 0, y: 1}
 })
 
 export function getInputDirection(){
